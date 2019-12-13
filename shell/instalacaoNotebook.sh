e8343dc376d8e8eadb2d5771e6e84dcb7088afb3
HLAM <hemilioaraujo@gmail.com>
2019-12-13 20:30:23 -0300

    add shell/instalacaoNotebook.sh (instalação inicial linux notebook)

---
+++ shell/instalacaoNotebook.sh
@@ -0,0 +1,59 @@
+!#/bin/bash
+# ATUALIZA O SISTEMA
+sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get upgrade && sudo apt-get autoremove;
+sudo apt-get install;
+# INSTALA FONTES MS
+sudo apt-get install ttf-mscorefonts-installer;
+# INSTALA VLC
+sudo apt-get install vlc;
+# ADICIONA REPOSITÓRIO ATOM
+sudo add-apt-repository ppa:webupd8team/atom;
+sudo apt-get update;
+# INSTALA ATOM
+sudo apt-get install atom;
+# INSTALA SNAP
+sudo apt-get install snap;
+# PYCHARM PELO SNAP
+sudo snap install pycharm-community --classic;
+# VISUAL STUDIO CODE PELO SNAP
+sudo snap install code --classic;
+# ADICIONAR REPOSITÓRIO UMAKE
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make;
+sudo apt-get update;
+sudo apt-get install ubuntu-make;
+# ARDUINO PELO UMAKE
+sudo umake ide arduino;
+# TKINTER
+sudo apt-get install python3-tk;
+# ATUALIZA O PIP
+pip3 install --upgrade pip;
+# KDENLIVE
+sudo apt-get install kdenlive;
+# GOOGLE CHROME
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list';
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
+sudo apt-get update;
+sudo apt-get install google-chrome-stable;
+# GIT
+add-apt-repository ppa:git-core/ppa;
+sudo apt-get update;
+sudo apt-get install git;
+# UNRAR
+sudo apt-get install unrar;
+# GIMP
+sudo apt-get install gimp;
+# JAVA
+sudo add-apt-repository ppa:webupd8team/java;
+sudo apt-get update;
+sudo apt-get install oracle-java8-installer;
+# PACOTES E GERENCIADORES BLUETOOTH
+sudo apt-get install bluetooth bluez bluez-tools rfkill -y;
+sudo apt-get install blueman -y && blueman-manager;
+# TELEGRAM
+sudo apt-get install telegram-desktop;
+# ZOOM - vídeo conferência
+sudo apt-get install zoom;
+# REMOVENDO PACOTES DESNECESSÁRIOS
+sudo apt-get autoremove;
+# REINICIA
+reboot;
