a45235dd2c8823755dbb76fbd3474ed16ed142ef
hemilioaraujo <hemilioaraujo@gmail.com>
2019-12-14 19:16:40 -0300

    adicionado o arquivo e alternada a instalação do atom de apt-get para snap

---
+++ shell/instalacaoDesktop.sh
@@ -0,0 +1,47 @@
+# ATUALIZA O SISTEMA
+sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get upgrade && sudo apt-get autoremove;
+sudo apt-get install;
+# INSTALA FONTES MS
+sudo apt-get install ttf-mscorefonts-installer;
+# INSTALA VLC
+sudo apt-get install vlc;
+# ADICIONA REPOSITÓRIO ATOM
+sudo add-apt-repository ppa:webupd8team/atom;
+sudo apt-get update;
+# INSTALA SNAP
+sudo apt-get install snap;
+# PYCHARM PELO SNAP
+sudo snap install pycharm-community --classic;
+# VISUAL STUDIO CODE PELO SNAP
+sudo snap install code --classic;
+# INSTALA ATOM
+sudo snap install atom;
+# ATUALIZA O PIP
+pip3 install --upgrade pip;
+# KDENLIVE
+sudo apt-get install kdenlive;
+# GOOGLE CHROME
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list';
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
+sudo apt-get update;
+sudo apt-get install google-chrome-stable;
+# GIT
+add-apt-repository ppa:git-core/ppa;
+sudo apt-get update;
+sudo apt-get install git;
+# UNRAR
+sudo apt-get install unrar;
+# GIMP
+sudo apt-get install gimp;
+# JAVA
+sudo add-apt-repository ppa:webupd8team/java;
+sudo apt-get update;
+sudo apt-get install oracle-java8-installer;
+# TELEGRAM
+sudo apt-get install telegram-desktop;
+# ZOOM - vídeo conferência
+sudo apt-get install zoom;
+# REMOVENDO PACOTES DESNECESSÁRIOS
+sudo apt-get autoremove;
+# REINICIA
+reboot;
