a6143ec57c41924e94390767f8710c2fecc7c93a
HLAM <hemilioaraujo@gmail.com>
2019-12-13 21:22:22 -0300

    add alias script (notebook)

---
+++ shell/aliasNotebook.sh
@@ -0,0 +1,24 @@
+#Mensagem d Boas Vindas:
+sudo echo "echo '   _      *** SE VOCÊ NÃO É HEMÍLIO O DEUS SUPREMO,   ***'"  >> "/etc/bash.bashrc";
+sudo echo "echo '  *v*     ***        SAIA IMEDIATAMENTE DE            ***'"  >> "/etc/bash.bashrc";
+sudo echo "echo ' /(_)\    ***        '`hostname`'            ***'"  >> "/etc/bash.bashrc";
+sudo echo "echo '  ^ ^     ***     '`date`'        ***'"  >> "/etc/bash.bashrc";
+sudo echo "echo ' '"  >> "/etc/bash.bashrc";
+
+# PING UOL
+sudo echo "alias pinguol='ping www.uol.com.br';" >> "/etc/bash.bashrc";
+
+# ALIAS DO APT-GET
+sudo echo "alias sins='sudo apt-get install';" >> "/etc/bash.bashrc";
+sudo echo "alias supd='sudo apt-get update';" >> "/etc/bash.bashrc";
+sudo echo "alias supg='sudo apt-get upgrade';" >> "/etc/bash.bashrc";
+sudo echo "alias surm='sudo apt-get autoremove';" >> "/etc/bash.bashrc";
+
+# ALIAS GIT
+sudo echo "alias gitc='git commit -m';" >> "/etc/bash.bashrc";
+sudo echo "alias gitps='git push';" >> "/etc/bash.bashrc";
+sudo echo "alias gitpl='git pull';" >> "/etc/bash.bashrc";
+sudo echo "alias gitf='git fetch';" >> "/etc/bash.bashrc";
+sudo echo "alias gits='git status';" >> "/etc/bash.bashrc";
+sudo echo "alias gita='git add';" >> "/etc/bash.bashrc";
+sudo echo "alias gitck='git checkout';" >> "/etc/bash.bashrc";
