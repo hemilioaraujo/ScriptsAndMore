d023208b863683ca32b1ae2b5ec82d957e714da8
hemilioaraujo <hemilioaraujo@gmail.com>
2020-10-02 21:37:14 -0300

    adicionado argumento -y e removido algumas instalações

--- shell/instalacaoNotebook.sh
+++ shell/instalacaoNotebook.sh
@@ -1,77 +1,43 @@
 !#/bin/bash
 # ATUALIZA O SISTEMA
-sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get upgrade && sudo apt-get autoremove;
-sudo apt-get install;
+sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt-get install -y;
 # INSTALA FONTES MS
-sudo apt-get install ttf-mscorefonts-installer;
+sudo apt-get install ttf-mscorefonts-installer -y;
 # INSTALA VLC
-sudo apt-get install vlc;
+sudo apt-get install vlc -y;
 # INSTALA SNAP
-sudo apt-get install snap;
+sudo apt-get install snap -y;
 # PYCHARM PELO SNAP
-sudo snap install pycharm-community --classic;
+sudo snap install pycharm-community --classic -y;
 # VISUAL STUDIO CODE PELO SNAP
-sudo snap install code --classic;
-# INSTALA ATOM
-sudo snap install atom;
-# ADICIONAR REPOSITÓRIO UMAKE
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make;
-sudo apt-get update;
-sudo apt-get install ubuntu-make;
-# ARDUINO PELO UMAKE
-sudo umake ide arduino;
-# INSTALLA IPYTHON
-sudo apt-get install ipython3;
-# TKINTER
-sudo apt-get install python3-tk;
-# ATUALIZA O PIP
-pip3 install --upgrade pip;
-# INSTALAR PYTHON3 VENV
-sudo apt-get install python3-venv;
+sudo snap install code --classic -y;
 # SQLITEBROWSER
-sudo apt-get install sqlitebrowser;
+sudo apt-get install sqlitebrowser -y;
 # KDENLIVE
-sudo apt-get install kdenlive;
-# GOOGLE CHROME
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list';
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
-sudo apt-get update;
-sudo apt-get install google-chrome-stable;
+sudo apt-get install kdenlive -y;
 # GIT
-add-apt-repository ppa:git-core/ppa;
-sudo apt-get update;
-sudo apt-get install git;
+add-apt-repository ppa:git-core/ppa -y && sudo apt-get update -y && sudo apt-get install git -y;
 # UNRAR
-sudo apt-get install unrar;
+sudo apt-get install unrar -y;
 # GIMP
-sudo apt-get install gimp;
+sudo apt-get install gimp -y;
 # JAVA
-sudo add-apt-repository ppa:linuxuprising/java;
-sudo apt update;
-sudo apt install oracle-java13-installer;
-sudo apt install oracle-java13-set-default;
+sudo add-apt-repository ppa:linuxuprising/java -y && sudo apt update -y && sudo apt install oracle-java13-installer -y && sudo apt install oracle-java13-set-default -y;
 # CODECS DE MÍDIA
-sudo apt-get install ubuntu-restricted-extras;
+sudo apt-get install ubuntu-restricted-extras -y;
 # UBUNTU TWEAK
-sudo apt install gnome-tweak-tool;
+sudo apt install gnome-tweak-tool -y;
 # GNOME SUSHI
-sudo apt install gnome-sushi;
+sudo apt install gnome-sushi -y;
 # PACOTES E GERENCIADORES BLUETOOTH
-sudo apt-get install bluetooth bluez bluez-tools rfkill -y;
-sudo apt-get install blueman -y && blueman-manager;
+sudo apt-get install bluetooth bluez bluez-tools rfkill -y && sudo apt-get install blueman blueman-manager -y;
 # TELEGRAM
-sudo apt-get install telegram-desktop;
+sudo apt-get install telegram-desktop -y;
 # ANKI
-sudo apt-get install anki;
+sudo apt-get install anki -y;
 # TREE
-sudo apt-get install tree;
-# ZOOM - vídeo conferência
-sudo apt-get install zoom;
-#INSTALAR WoeUSB - CRIADOR PENDRIVE BOOTAVEL WINDOWS
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y;
-sudo apt-get update;
-sudo apt-get install woeusb -y;
+sudo apt-get install tree -y;
 # REMOVENDO PACOTES DESNECESSÁRIOS
-sudo apt-get autoremove;
+sudo apt-get autoremove -y;
 # REINICIA
-reboot;
+reboot -y;
