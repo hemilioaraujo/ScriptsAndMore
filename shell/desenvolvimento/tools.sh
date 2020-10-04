fc7d5830eb2997a76b1154de917c30fb544ecb23
hemilioaraujo <hemilioaraujo@gmail.com>
2020-10-03 21:36:27 -0300

    automatizada instalaçao das tools

---
+++ shell/desenvolvimento/tools.sh
@@ -0,0 +1,6 @@
+# SQLITEBROWSER
+sudo apt-get install sqlitebrowser -y;
+# DBEAVER
+sudo snap install dbeaver-ce;
+# GIT
+add-apt-repository ppa:git-core/ppa -y && sudo apt-get update -y && sudo apt-get install git -y;
