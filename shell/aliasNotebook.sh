07c8cbf3ddd9d4d9021d45f6005233707f7039ee
HLAM <hemilioaraujo@gmail.com>
2020-12-17 19:23:21 -0300

    editado manage.py

--- shell/aliasNotebook.sh
+++ shell/aliasNotebook.sh
@@ -24,7 +24,7 @@ sudo echo "alias gita='git add';" >> "/etc/bash.bashrc";
 sudo echo "alias gitck='git checkout';" >> "/etc/bash.bashrc";
 
 # ALIAS DJANGO/PYTHON
-sudo echo "alias pymg='python manage.py';" >> "/etc/bash.bashrc";
+sudo echo "alias pymg='python $VIRTUAL_ENV/../manage.py';" >> "/etc/bash.bashrc";
 
 # ALIAS VENV
 sudo echo "alias srcv='source venv/bin/activate';" >> "/etc/bash.bashrc";
